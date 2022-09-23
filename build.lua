775ea8f682f0e497af26deff5f708d6f8bcf1535
Yegor Bugayenko <yegor256@gmail.com>
2022-09-23 18:46:17 +0300

    #12: l3build

---
+++ build.lua
@@ -0,0 +1,20 @@
+module = "iexec"
+uploadconfig = {
+  pkg = "iexec",
+  version = "0.0.0",
+  author = "Yegor Bugayenko",
+  uploader = "Yegor Bugayenko",
+  email = "yegor256@gmail.com",
+  note = "Bug fixes",
+  announcement = "",
+  ctanPath = "/macros/latex/contrib/iexec",
+  bugtracker = "https://github.com/yegor256/iexec/issues",
+  home = "https://github.com/yegor256/iexec",
+  description = "With the help of the \\iexec command, you can execute a shell command and then input its output into your document. This package also lets you use any special symbols inside your command.",
+  development = "https://github.com/yegor256/iexec",
+  license = "mit",
+  summary = "Execute shell commands and input their output",
+  repository = "https://github.com/yegor256/iexec",
+  support = "https://github.com/yegor256/iexec",
+  topic = "verbatim"
+}
