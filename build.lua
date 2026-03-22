cd2f10ca014fdf1af0d0f4f8c0e5b87a3b71b392
Yegor Bugayenko <yegor256@gmail.com>
2026-03-22 18:15:56 +0300

    version with date

--- build.lua
+++ build.lua
@@ -11,7 +11,7 @@ typesetruns = 2
 
 uploadconfig = {
   pkg = "iexec",
-  version = "0.0.0",
+  version = "0.0.0 0000/00/00",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
